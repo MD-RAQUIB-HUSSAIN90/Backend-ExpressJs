df58cc85371e2a470f023ee94e3cd64760289bf8
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-09-16 00:24:44 +0530

    last commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 let port = 8000;
 
+app.listen(port, () => {
+  console.log(`app listening on port  ${port}`);
+});
+
 // app.use((req, res) => {
 //   console.log("request was received");
 // //   res.send("this is a basic response");
@@ -12,23 +16,24 @@ let port = 8000;
 // });
 
 app.get("/", (req, res) => {
-  res.send("this is a root path");
+  res.send("Hello this is a root path");
 });
 app.get("/apple", (req, res) => {
-  res.send("this is a apple path");
+  res.send("This is a apple path");
 });
 app.get("/find", (req, res) => {
-  res.send("this is a find path");
+  res.send("This is a find path");
 });
 app.get("/search", (req, res) => {
-  res.send("this is a search path");
+  res.send("This is a search path");
 });
-app.get("*unknownPage", (req, res) => {
-  res.send("this is a 404 path");
-});
+// app.get("*unknownPage", (req, res) => {
+//   res.send("This is a 404 path");
+// });
 app.post("/", (req, res) => {
+  res.send("This is a post request");
 });
-
-app.listen(port, () => {
-  console.log(`app listening on port  ${port}`);
+app.get("/:username/:id/:age", (req, res) => {
+  console.log(req.params);
+  res.send(`<h1>User: ${req.params.username}</h1><p>ID: ${req.params.id}</p><p>Age: ${req.params.age}</p>`);
 });
