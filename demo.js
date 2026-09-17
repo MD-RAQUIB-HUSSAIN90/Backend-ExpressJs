bf6c9e3411c88fe1f1716594af99c5936177776b
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-09-17 20:49:03 +0530

    last commit

--- demo.js
+++ demo.js
@@ -6,16 +6,34 @@ app.listen(port, () => {
   console.log("server is live on port " + port);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello, World!");
+// });
+// app.get("/search", (req, res) => {
+//   res.send("Search whatever you want!");
+// });
+// app.get("/about", (req, res) => {
+//   res.send("About us");
+// });
+// app.get("/contact/:username", (req, res) => {
+//     console.log(req.params)
+//   res.send("Contact with "+ req.params.username);
+// });
+
+// const fs = require("fs");
+
+// fs.readFile("hello.txt", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("File readed:");
+//   }
+// });
+
 app.get("/search", (req, res) => {
-  res.send("Search whatever you want!");
-});
-app.get("/about", (req, res) => {
-  res.send("About us");
-});
-app.get("/contact/:username", (req, res) => {
-    console.log(req.params)
-  res.send("Contact with "+ req.params.username);
+  let { q } = req.query;
+  if (!q) {
+    res.send("<h1>Search query is missing</h1>");
+  }
+  res.send(`<h1>Search results for query: ${q}</h1>`);
 });
